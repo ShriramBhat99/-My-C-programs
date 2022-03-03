e7741381f895ac8e5ec406eda3fce7652e687aca
ShriramBhat <bhatshriram9@gamil.com>
2022-03-03 14:38:21 +0530

    Commit

---
+++ Swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("Enter first number\n");
+    scanf("%d",&a);
+    printf("Enter Seconf number\n");
+    scanf("%d",&b);
+    printf("Before Swapping\n");
+    printf("First number A=%d\n Second number B=%d\n",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After Swapping\n");
+}   printf("First number A=%d\n Second number B=%d\n",a,b);
