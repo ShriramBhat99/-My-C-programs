c4e3752f2acbb14dfe1fe2d789f40c565fd630d6
ShriramBhat <bhatshriram9@gamil.com>
2022-03-03 14:22:28 +0530

    Second Program

---
+++ StarPatter1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+    int  n,i,j,k;
+    clrscr();
+    printf("Enter the number of lines\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+         for(j=i;j<=n;j++)
+             printf(" ");
+         for(k=1;k<=i;k++)
+             printf("*");
+         printf("\n");
+     }
+     getch();
+ }
+
+
