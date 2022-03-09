2886e9661d483459cd2077aa08470d938266aad4
ShriramBhat <bhatshriram9@gamil.com>
2022-03-09 19:48:57 +0530

    Commit

---
+++ StarPattern3.c
@@ -0,0 +1,34 @@
+
+/* C Program to Print the following star pattern
+                            *
+                           * *
+                          *   *
+                         * * * *
+                                       */
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,j,i,k;
+    clrscr();
+    printf("Enter the Number of Rows\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=n-i;j>=1;j--)
+        printf(" ");
+        printf("*");
+        if(i>1 && i!=n)
+        {
+           for(k=1;k<i;k++)
+               printf("  ");
+          printf("*");
+        }
+        if(i==n)
+           for(i=1;i<n;i++)
+            printf(" *");
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
