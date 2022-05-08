d4df0d7d27efdd16be2c5ee381aefba7131d45e9
Shriram <bhatshriram9@gmail.com>
2022-05-08 15:57:58 +0530

    Commit

---
+++ PrimeArray.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[20],n,i,j,sum=0,flag=0;
+    clrscr();
+    printf("Enter the number of elements\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(i=0;i<n;scanf("%d",&a[i++]));
+    printf("Sum of ");
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==1) flag=1;
+        for(j=2;j<=(a[i]/2);j++)
+              if(a[i]%j==0)
+              {
+                  flag=1;
+                  break;
+              }
+        if(flag==0)
+        {
+            printf("%d ",a[i]);
+            sum=sum+a[i];
+        }
+        flag=0;
+
+    }
+    printf("is  %d\n",sum);
+    return 0;
+    getch();
+
+}
